9e08d115156cac43ba5185fbbe92a33ed442826f
Anitha S <anitha.subramani@maktoinc.com>
2019-01-10 13:59:46 +0530

    Initial commit

---
+++ docs/readme.md
@@ -0,0 +1,7 @@
+Welcome to the project documentation!
+
+Use `npm run docs` for easier navigation.
+
+## Available documentation
+
+[[index]]
---
+++ src/app/component/component.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BarChartComponent } from './chart/bar-chart/bar-chart.component';
+
+@NgModule({
+  declarations: [
+    BarChartComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentModule { }
